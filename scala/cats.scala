6f1da39abff72ec954c96817f6e4e7c022c78c53
Valeriy <valeriy@manenkov.com>
2021-12-25 13:33:27 +0300

    scala: cats: improve crdt, abstract KV

--- scala/cats.scala
+++ scala/cats.scala
@@ -479,12 +479,58 @@ checkLogin(4, "davinci").run(db)
 
 // CRDT
 // ----
+
 import cats.instances.list._   // for Monoid
 import cats.instances.map._    // for Monoid
 import cats.syntax.semigroup._ // for |+|
 import cats.syntax.foldable._  // for combineAll
 import cats.instances.int._ // for Monoid
 import cats.kernel.CommutativeMonoid
+
+trait KeyValueStore[F[_,_]] {
+  def put[K, V](f: F[K, V])(k: K, v: V): F[K, V]
+
+  def get[K, V](f: F[K, V])(k: K): Option[V]
+
+  def getOrElse[K, V](f: F[K, V])(k: K, default: V): V =
+    get(f)(k).getOrElse(default)
+
+  def values[K, V](f: F[K, V]): List[V]
+}
+
+object KeyValueStore {
+  implicit val mapKeyValueStoreInstance: KeyValueStore[Map] =
+  new KeyValueStore[Map] {
+    def put[K, V](f: Map[K, V])(k: K, v: V): Map[K, V] =
+      f + (k -> v)
+
+    def get[K, V](f: Map[K, V])(k: K): Option[V] =
+      f.get(k)
+
+    override def getOrElse[K, V](f: Map[K, V])
+        (k: K, default: V): V =
+      f.getOrElse(k, default)
+
+    def values[K, V](f: Map[K, V]): List[V] =
+      f.values.toList
+  }
+}
+
+implicit class KvsOps[F[_,_], K, V](f: F[K, V]) {
+  def put(key: K, value: V)
+        (implicit kvs: KeyValueStore[F]): F[K, V] =
+    kvs.put(f)(key, value)
+
+  def get(key: K)(implicit kvs: KeyValueStore[F]): Option[V] =
+    kvs.get(f)(key)
+
+  def getOrElse(key: K, default: V)
+        (implicit kvs: KeyValueStore[F]): V =
+    kvs.getOrElse(f)(key, default)
+
+  def values(implicit kvs: KeyValueStore[F]): List[V] =
+    kvs.values(f)
+}
 
 trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
   def combine(a1: A, a2: A): A
