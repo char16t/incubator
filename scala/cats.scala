4d3e5a7894d4a61649e22fa254c6aaad493c71ae
Valeriy <valeriy@manenkov.com>
2021-12-25 13:09:33 +0300

    scala: cats crdt simple implementation

--- scala/cats.scala
+++ scala/cats.scala
@@ -474,3 +474,85 @@ checkLogin(4, "davinci").run(db)
 // -----
 
 // ...
+
+
+
+// CRDT
+// ----
+import cats.instances.list._   // for Monoid
+import cats.instances.map._    // for Monoid
+import cats.syntax.semigroup._ // for |+|
+import cats.syntax.foldable._  // for combineAll
+import cats.instances.int._ // for Monoid
+import cats.kernel.CommutativeMonoid
+
+trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
+  def combine(a1: A, a2: A): A
+  def empty: A
+}
+
+object BoundedSemiLattice {
+  implicit val intInstance: BoundedSemiLattice[Int] =
+    new BoundedSemiLattice[Int] {
+      def combine(a1: Int, a2: Int): Int =
+        a1 max a2
+
+      val empty: Int =
+        0
+    }
+
+  implicit def setInstance[A]: BoundedSemiLattice[Set[A]] =
+    new BoundedSemiLattice[Set[A]]{
+      def combine(a1: Set[A], a2: Set[A]): Set[A] =
+        a1 union a2
+
+      val empty: Set[A] =
+        Set.empty[A]
+    }
+}
+
+trait GCounter[F[_,_],K, V] {
+  def increment(f: F[K, V])(k: K, v: V)
+        (implicit m: CommutativeMonoid[V]): F[K, V]
+
+  def merge(f1: F[K, V], f2: F[K, V])
+        (implicit b: BoundedSemiLattice[V]): F[K, V]
+
+  def total(f: F[K, V])
+        (implicit m: CommutativeMonoid[V]): V
+}
+
+object GCounter {
+  def apply[F[_,_], K, V]
+        (implicit counter: GCounter[F, K, V]) = counter
+  
+  implicit def mapGCounterInstance[K, V]: GCounter[Map, K, V] =
+  new GCounter[Map, K, V] {
+    def increment(map: Map[K, V])(key: K, value: V)
+          (implicit m: CommutativeMonoid[V]): Map[K, V] = {
+      val total = map.getOrElse(key, m.empty) |+| value
+      map + (key -> total)
+    }
+
+    def merge(map1: Map[K, V], map2: Map[K, V])
+          (implicit b: BoundedSemiLattice[V]): Map[K, V] =
+      map1 |+| map2
+
+    def total(map: Map[K, V])
+        (implicit m: CommutativeMonoid[V]): V =
+      map.values.toList.combineAll
+  }
+}
+
+val g1 = Map("a" -> 7, "b" -> 3)
+val g2 = Map("a" -> 2, "b" -> 5)
+
+val counter = GCounter[Map, String, Int]
+
+val merged = counter.merge(g1, g2)
+// merged: Map[String, Int] = Map("a" -> 7, "b" -> 5)
+merged
+
+val total  = counter.total(merged)
+// total: Int = 12
+total
