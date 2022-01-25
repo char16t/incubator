e07cd99ab62e157fa3b4827419d61499e807d17a
Valeriy Manenkov <valeriy@manenkov.com>
2022-01-25 12:20:01 +0300

    Add unfinished/demo

---
+++ unfinished/demo/server/src/main/scala/com/manenkov/demo/JsonFormats.scala
@@ -0,0 +1,15 @@
+package com.manenkov.demo
+
+import com.manenkov.demo.UserRegistry.ActionPerformed
+
+import spray.json.DefaultJsonProtocol
+
+object JsonFormats  {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+  import DefaultJsonProtocol._
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
