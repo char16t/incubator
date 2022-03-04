2c50aa72a732d834689a8f10d45c5897a637c431
Valeriy <valeriy@manenkov.com>
2022-03-04 13:29:58 +0300

    sql: versioning: delete implementation

--- sql/versioning.sql
+++ sql/versioning.sql
@@ -4,7 +4,8 @@ create table t7(
   entity_id bigserial not null,
   title varchar(128),
   created_at timestamp default now(),
-  updated_at timestamp default null
+  updated_at timestamp default null,
+  deleted boolean default false
 );
 
 -- insert
@@ -20,5 +21,14 @@ select id, 2, 'BBB2' from t7 where entity_id=2 order by created_at desc limit 1;
 insert into t7(prev_id, entity_id, title)
 select id, 2, 'BBB3' from t7 where entity_id=2 order by created_at desc limit 1;
 
+-- delete
+update t7
+set
+deleted=true
+from(select * from t7 where entity_id=2 and updated_at is null and deleted=false order by created_at desc limit 1) as last_row
+where t7.id = last_row.id
+;
+
 -- select
 select * from t7 where entity_id=2 and updated_at is null order by created_at desc limit 1;
+
