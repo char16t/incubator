40856644ce6707db1262072deb90a45d39dc0149
Valeriy Manenkov <valeriy@manenkov.com>
2022-05-08 13:16:01 +0300

    scala: extract tasks

--- typelevel-ecosystem/typelevel-ecosystem/src/main/scala/com/manenkov/sandbox/typelevel/Main.scala
+++ typelevel-ecosystem/typelevel-ecosystem/src/main/scala/com/manenkov/sandbox/typelevel/Main.scala
@@ -17,19 +17,21 @@ import scala.concurrent.duration.*
 
 object Main extends IOApp.Simple {
 
-  implicit def logger[F[_]: Sync]: Logger[F] = Slf4jLogger.getLogger[F]
+  implicit def logger[F[_] : Sync]: Logger[F] = Slf4jLogger.getLogger[F]
 
-  def say1[F[_]: Sync: Logger]: F[String] = for {
+  def say1[F[_] : Sync : Logger]: F[String] = for {
     _ <- debug">> say1"
     r <- Sync[F].delay("Hello, ")
     _ <- debug"<< say1 = $r"
   } yield r
-  def say2[F[_]: Sync: Logger]: F[String] = for {
+
+  def say2[F[_] : Sync : Logger]: F[String] = for {
     _ <- debug">> say2"
     r <- Sync[F].delay("Cats!")
     _ <- debug"<< say2 = $r"
   } yield r
-  def say3[F[_]: Sync: Logger]: F[String] = for {
+
+  def say3[F[_] : Sync : Logger]: F[String] = for {
     a <- say1[F]
     _ <- info"Value of A is \"$a\""
     b <- say2[F]
@@ -40,10 +42,18 @@ object Main extends IOApp.Simple {
 
   val oddSeconds: CalEvent = CalEvent.unsafe("*-*-* *:*:1/2")
   val everyFourSeconds: CalEvent = CalEvent.unsafe("*-*-* *:*:0/4")
-  def calevScheduler[F[_]: Temporal: Sync]: Scheduler[F, CalEvent] = CalevScheduler.systemDefault[F]
-  def calevScheduledTasks[F[_]: Temporal: Sync: Logger]: Stream[F, Unit] = calevScheduler[F].schedule(List(
-    oddSeconds      ->  Stream.eval {Sync[F].delay(println(LocalTime.now.toString + " task 1"))},
-    everyFourSeconds -> Stream.eval {Sync[F].delay(println(LocalTime.now.toString + " task 2"))},
+
+  def calevScheduler[F[_] : Temporal : Sync]: Scheduler[F, CalEvent] = CalevScheduler.systemDefault[F]
+
+  def task1[F[_]: Sync]: F[Unit] =
+    Sync[F].delay(println(LocalTime.now.toString + " task 1"))
+
+  def task2[F[_]: Sync]: F[Unit] =
+      Sync[F].delay(println(LocalTime.now.toString + " task 2"))
+
+  def calevScheduledTasks[F[_] : Temporal : Sync]: Stream[F, Unit] = calevScheduler[F].schedule(List(
+    oddSeconds -> Stream.eval {task1[F]},
+    everyFourSeconds -> Stream.eval {task2[F]},
   ))
 
   val run: IO[Unit] = for {
