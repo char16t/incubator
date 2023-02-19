3fad93551200ff96ce72a26913b407b9efa059f3
Valeriy <valeriy@manenkov.com>
2023-02-19 17:42:30 +0300

    Create en-us-locale.ps1

---
+++ windows/en-us-locale.ps1
@@ -0,0 +1,6 @@
+Set-Culture en-US
+Set-WinSystemLocale -SystemLocale en-US
+Set-WinUILanguageOverride -Language en-US
+Set-WinUserLanguageList en-US -Force
+Set-WinHomeLocation -GeoId 244
+shutdown -r;exit
