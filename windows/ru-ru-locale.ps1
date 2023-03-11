b93531036ad98a6ed2985feb7507ff071a362b5f
Valeriy <valeriy@manenkov.com>
2023-03-11 14:22:47 +0300

    Add ru-ru-locale.ps1

---
+++ windows/ru-ru-locale.ps1
@@ -0,0 +1,6 @@
+Set-Culture ru-RU
+Set-WinSystemLocale -SystemLocale ru-RU
+Set-WinUILanguageOverride -Language ru-RU
+Set-WinUserLanguageList ru-RU -Force
+Set-WinHomeLocation -GeoId 203
+shutdown -r;exit
